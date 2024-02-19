eac4521158f6f5c90451dbb712249570e299a750
nuocmam75009 <you@example.com>
2024-02-19 04:56:00 -0800

    five

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/* include stdio h library to use print
+*/
+#include <stdio.h>
+int main(void)
+{
+	/* using printf to print
+	*/
+	printf("with proper grammar, but the outcome is a piece of art,");
+		/* Return is success */
+		return (0);
+}
