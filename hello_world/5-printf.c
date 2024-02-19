21c678b4b658c19afc155543ae403711f2b7ba7b
nuocmam75009 <you@example.com>
2024-02-19 05:31:12 -0800

    betty ta mere la pute

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,13 @@
-/** 
- * include stdio h library to use print
- */
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	/* using printf to print */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		/* Return is success */
-		return (0);
+	/* Return is success */
+	return (0);
 }
