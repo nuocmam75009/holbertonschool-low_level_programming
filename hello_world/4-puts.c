0ce4966ff93ef70328e92b375cf681a4b199e700
nuocmam75009 <you@example.com>
2024-02-19 04:05:01 -0800

    fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /** 
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: Always  (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like bulding a multilingual puzzle\n");
-	return (O);
+	return (0);
 }
