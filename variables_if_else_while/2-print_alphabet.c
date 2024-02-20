33e10eb117a2a7509be70d5adb3ee845abbae8c1
nuocmam75009 <you@example.com>
2024-02-20 02:44:08 -0800

    fini

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: always zero (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		putchar(ch);
+
+	return (0);
+}
