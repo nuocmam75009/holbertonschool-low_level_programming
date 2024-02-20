f840351616c9c42c7efcea70bf42e37bbb7a1eff
nuocmam75009 <you@example.com>
2024-02-20 01:20:43 -0800

    first task

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwaus 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive \n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative \n", n);
+	}
+	else
+	{
+		printf("%d is zero \n", n);
+	}
+	return (0);
+}}
