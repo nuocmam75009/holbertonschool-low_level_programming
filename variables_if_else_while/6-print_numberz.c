a36d232d7e183f11501d20083a825772bfd277fc
nuocmam75009 <you@example.com>
2024-02-20 04:46:03 -0800

    fixed

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,11 @@
  */
 int main(void)
 {
-	char ch = 0;
+	int num = 0;
 
-	for (ch = 0; ch < 10; ch++)
-		putchar(ch);
+	for (num = 0; num < 10; num++)
+		putchar((num %10) + '0');
+
 	putchar('\n');
 
 	return (0);
