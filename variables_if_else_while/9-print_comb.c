b14bf25ddd137e6e7275a1f7fc62327e3d519dae
nuocmam75009 <you@example.com>
2024-02-20 06:15:36 -0800

    betty fdp

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - ENTRY - prints combinations of single digits
+ *
+ * Return always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		putchar((number % 10) + '0');
+		if (number == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar ('\n');
+	return (0);
+
+}
