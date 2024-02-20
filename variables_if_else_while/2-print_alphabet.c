76a57464f470609f0bbbc3e410127902b9a54dc7
nuocmam75009 <you@example.com>
2024-02-20 02:50:13 -0800

    fix

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main - void entry
  *
  * Return: always zero (success)
  */
 int main(void)
 {
 	char ch = 'a';
-	for (ch = 'a'; ch <= 'z'; ch++)
 
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
 	return (0);
