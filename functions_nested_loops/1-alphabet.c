328a980e9ec152127830fb596775c620f9a244b1
Lucas <sayhi.kiseiju@gmail.com>
2024-02-21 23:52:21 +0100

    fixed betty for second task

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,13 @@
 #include "main.h"
-/*
- * main - prints alphabet
+
+/**
+ * print_alphabet - function that prints alphabet
+ * the function prints the alphabet
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet(void) /* ta mère*/
+/* the function prints the alphabet*/
 {
 	char alphabet;
 
