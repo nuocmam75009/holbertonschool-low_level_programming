1c79a092625ba867e3556f5b3eebf6cc08ab62e8
Lucas <sayhi.kiseiju@gmail.com>
2024-02-26 10:40:38 +0100

    checks for uppercase

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
-/** 
+/**
  * _isupper - checks for uppercase char
- * @c: character being checked 
+ * @c: character being checked
  *
  * Return: 1 if uppercase,
  * 0 otherwise
