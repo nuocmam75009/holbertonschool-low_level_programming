e8a3e544a7347ccb4cddf8789d4b7d6e51ad5aeb
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-02-26 15:10:52 +0100

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,9 +7,9 @@ void print_square(int size)
 {
 	int rows, columns;
 
-	for (columns = 1; columns < size; columns++)
+	for (columns = 0; columns < size; columns++)
 	{
-		for (rows = 1; rows < size; rows++)
+		for (rows = 0; rows < size; rows++)
 			_putchar('#');
 		_putchar('\n');
 	}
