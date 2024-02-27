23742be4f20e9aaa65100e83eb80936c17593f0a
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-02-27 10:49:47 +0100

    FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,19 @@ int main(void)
 	for (number = 1; number <= 100; number++)
 	{
 		if ((number % 15) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 
 		else if ((number % 3) == 0)
-			printf("Fizz ");
+			printf("Fizz");
 
 		else if ((number % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 
 		else
-			printf("%d ", number);
-		
-		if (number == 100)
-			printf("Buzz");
+			printf("%d", number);
+
+		if (number < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
