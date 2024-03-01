5221a81bef68dd3aaf66e82a1293bc988caaed07
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-03-01 11:07:55 +0100

    task

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,18 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i, j, len;
 
-	while (s[index++])
-		len++;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+		_putchar(s[j]);
 
 	_putchar('\n');
 }
