98eb1a6cd6f383dce070d5da596cb2ee06681ca0
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-03-08 17:25:30 +0100

    tasks 1-7

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,12 +5,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\O')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 
-	else if (s[0] != '\0')
+	else if	(s[0] != '\0')
 	{
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
